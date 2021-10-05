8edcf12d87d6e427c7bb38c41c32776cfaf41cd9
Mech <mechsix@users.noreply.github.com>
2021-10-05 23:49:51 +0800

    feat: Scene 2, the 3 gress box

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,29 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {Mesh, MeshBasicMaterial, MeshStandardMaterial, TextureLoader, CanvasTexture} from 'three';
-import { Canvas, useFrame } from '@react-three/fiber';
+import React, {useRef} from 'react';
+import {
+  Mesh,
+  Camera,
+  Scene,
+  MeshStandardMaterial,
+  TextureLoader,
+} from 'three';
+import {
+  useFrame,
+} from '@react-three/fiber';
+import {
+  PerspectiveCamera,
+  OrbitControls,
+  useFBO,
+} from '@react-three/drei';
 import GrassImage from './material/grass.jpg';
 import './App.css';
 
-const Box: React.FC = () => {
+interface BoxProps {
+  position: [x: number, y: number, z: number]
+}
+
+const Box: React.FC<BoxProps> = (props) => {
+  const {position} = props
   const ref = useRef<Mesh>();
-  const [active, setActive] = useState(false);
 
   const loader = new TextureLoader();
   const grassMaterial = new MeshStandardMaterial({
@@ -15,42 +32,56 @@ const Box: React.FC = () => {
     metalness: 0.7,
   })
 
-  const solidOrangeMaterial = new MeshStandardMaterial({
-    color: 'orange',
-    roughness: 0.5,
-    metalness: 0.7,
-  })
-
   useFrame(() => {
     if (ref?.current !== undefined) {
       ref.current.rotation.x += 0.003;
-      ref.current.rotation.y += 0.003;
-      ref.current.rotation.z += 0.002;
+      // ref.current.rotation.y += 0.003;
+      // ref.current.rotation.z += 0.002;
     }
   });
 
-  const handleClick = () => {
-    setActive(!active);
-  };
-
   return (
     <mesh
+      position={position}
       ref={ref}
       scale={1}
-      onClick={handleClick}
-      material={active ? grassMaterial : solidOrangeMaterial}
+      material={grassMaterial}
     >
-      <icosahedronGeometry args={[2, 1]} />
+      <boxGeometry args={[2, 2, 2]} />
     </mesh>
   );
 };
 
-const App: React.FC = () => (
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box/>
-  </Canvas>
-);
+const App: React.FC = () => {
+  const virtualCamera = React.useRef<Camera>()
+  const fbo = useFBO(150, 150)
+  const [virtualScene] = React.useState(() => new Scene())
+
+  useFrame(({ gl }) => {
+    if (virtualCamera.current) {
+      gl.setRenderTarget(fbo)
+      gl.render(virtualScene, virtualCamera.current)
+
+      gl.setRenderTarget(null)
+    }
+  })
+
+  return (
+    <>
+      <PerspectiveCamera ref={virtualCamera} />
+      <ambientLight />
+      <pointLight position={[0, 0, 10]} />
+      <Box position={[-3, 0, 3]}/>
+      <Box position={[0, 0, -3]}/>
+      <Box position={[3, 0, 0]}/>
+      <OrbitControls
+        camera={virtualCamera.current}
+        enablePan={true}
+        enableZoom={true}
+        enableRotate={true}
+      />
+    </>
+  );
+}
 
 export default App;
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Canvas } from '@react-three/fiber';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Canvas>
+      <App />
+    </Canvas>
   </React.StrictMode>,
   document.getElementById('root'),
 );
